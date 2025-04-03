a1f163a1cfb4c11b448a97a48bc92bd8b8b9d37e
Raahim Fareed <raahimfareed@proton.me>
2025-04-03 10:45:14 +0500

    indentation

--- main.py
+++ main.py
@@ -4,35 +4,35 @@ import discord
 from dotenv import load_dotenv
 
 cogs_list = [
-  'hello',
-  'moderation'
+    'hello',
+    'moderation'
 ]
 
 
 def init():
-  load_dotenv()
+    load_dotenv()
 
 def main():
-  init()
+    init()
 
-  token = str(os.getenv("TOKEN"))
-  bot = discord.Bot()
+    token = str(os.getenv("TOKEN"))
+    bot = discord.Bot()
 
 
-  @bot.event
-  async def on_ready():
-    print(f"{bot.user} is ready and online")
+    @bot.event
+    async def on_ready():
+        print(f"{bot.user} is ready and online")
 
-  @bot.event
-  async def on_member_join(member):
-    await member.send('Welcome to NeoFlux')
+    @bot.event
+    async def on_member_join(member):
+        await member.send('Welcome to NeoFlux')
 
-  
-  for cog in cogs_list:
-    bot.load_extension(f"cogs.{cog}")
-  
-  bot.run(token)
+
+    for cog in cogs_list:
+        bot.load_extension(f"cogs.{cog}")
+
+    bot.run(token)
 
 
 if __name__ == "__main__":
-  main()
+    main()
