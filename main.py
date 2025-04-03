5495470ec1db4e0ef8c4150a284d11fdee9312df
Raahim Fareed <raahimfareed@proton.me>
2025-04-03 17:07:05 +0500

    added activity, command error and meetings cog

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import os
 
 import discord
+from discord.ext.commands import MissingPermissions
+from discord.types import activity
 from dotenv import load_dotenv
 
 cogs_list = [
     'hello',
-    'moderation'
+    'moderation',
+    'meeting',
 ]
 
 
@@ -26,12 +29,19 @@ def main():
 
     @bot.event
     async def on_ready():
+        game = discord.Game('with the API')
+        await bot.change_presence(activity=game)
         print(f"{bot.user} is ready and online")
 
     @bot.event
     async def on_member_join(member):
         await member.send('Welcome to NeoFlux')
 
+    @bot.event
+    async def on_application_command_error(ctx: discord.ApplicationContext, error):
+        if isinstance(error, MissingPermissions):
+            await ctx.respond("You don't have permissions to run this command! :no_entry:")
+        raise error
 
     for cog in cogs_list:
         bot.load_extension(f"cogs.{cog}")
